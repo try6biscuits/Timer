6d2b2688943f63805e30675c7a9d8b823a5d0e04
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-02-17 21:51:36 -0600

    add new Date function:

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 const userInput = document.getElementById("userInput");
 const submitBtn = document.querySelector(".submitBtn");
 
-
-
+let today = new Date();
+let time = today.getHours(21) + ":" + today.getMinutes(49) + ":" + today.getSeconds(0);
+console.log(time)
