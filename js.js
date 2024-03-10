d172057d066a090a93a6ad0d1a2fb0c59b56575c
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-03-10 18:13:45 -0500

    submit function into 24 hour function

--- js.js
+++ js.js
@@ -7,43 +7,43 @@ let hours = today.getHours(21);
 let minutes = today.getMinutes(11);
 let seconds = today.getSeconds(39);
 
-submitBtn.addEventListener("click", function () {
-  const inputValue = userInput.value;
-  if (isNaN(inputValue) || inputValue === undefined) {
-    alert("Enter a number");
-  } else {
-    if (inputValue == hours) {
-      alert("You are 0 Hours away from " + hours + " So therefor its " + hours);
-    } else if (inputValue < hours) {
-      let results = inputValue - hours + 24;
-      alert(
-        "You are " +
-          results +
-          " Hours from " +
-          inputValue +
-          " so therefor your time is " +
-          hours
-      );
-    } else if (inputValue > hours) {
-      let results = inputValue - hours;
-      alert(
-        "You are " +
-          results +
-          " Hours from " +
-          inputValue +
-          " so therefor your time is " +
-          hours
-      );
-    }
-  }
-});
-
-function ifClickedTwelveHourBtn(){
-  alert("You clicked the 12 hour button")
+function ifClickedTwelveHourBtn() {
+  alert("You clicked the 12 hour button");
 }
 twelveHourBtn.addEventListener("click", ifClickedTwelveHourBtn);
 
 function ifClickedTwentyFourHourBtn() {
-  alert("You clicked the 24 hour button");
+  submitBtn.addEventListener("click", function () {
+    const inputValue = userInput.value;
+    if (isNaN(inputValue) || inputValue === undefined) {
+      alert("Enter a number");
+    } else {
+      if (inputValue == hours) {
+        alert(
+          "You are 0 Hours away from " + hours + " So therefor its " + hours
+        );
+      } else if (inputValue < hours) {
+        let results = inputValue - hours + 24;
+        alert(
+          "You are " +
+            results +
+            " Hours from " +
+            inputValue +
+            " so therefor your time is " +
+            hours
+        );
+      } else if (inputValue > hours) {
+        let results = inputValue - hours;
+        alert(
+          "You are " +
+            results +
+            " Hours from " +
+            inputValue +
+            " so therefor your time is " +
+            hours
+        );
+      }
+    }
+  });
 }
 twentyFourHourtBtn.addEventListener("click", ifClickedTwentyFourHourBtn);
