89a16e09ae46ab6469f3b68f66fa6c68778b37a0
Brandon Ngo <Brandon.ngo.inbox@gmail.com>
2024-03-10 17:29:20 -0500

    selected 12 and 24 hour btn

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const userInput = document.getElementById("userInput");
 const submitBtn = document.querySelector(".submitBtn");
-
+const twentyFourHourtBtn = document.querySelector(".24-hour-btn");
+const twelveHourBtn = document.querySelector(".12-hour-btn");
 let today = new Date();
 let hours = today.getHours(21);
 let minutes = today.getMinutes(11);
